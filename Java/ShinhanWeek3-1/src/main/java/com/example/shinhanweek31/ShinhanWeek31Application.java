5df11b80605a0d3a1e3c34d581fd4f1c515d1c3f
thainta <thai1612200110a@gmail.com>
2023-01-04 19:31:58 +0700

    homework

---
+++ Java/ShinhanWeek3-1/src/main/java/com/example/shinhanweek31/ShinhanWeek31Application.java
@@ -0,0 +1,13 @@
+package com.example.shinhanweek31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhanWeek31Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShinhanWeek31Application.class, args);
+    }
+
+}
---
+++ Java/ShinhanWeek3-1/src/test/java/com/example/shinhanweek31/ShinhanWeek31ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shinhanweek31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinhanWeek31ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
